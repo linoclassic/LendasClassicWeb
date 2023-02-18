66ae200e80264cf129c6fdcbc4f8dc5356862670
VICTOR LINO SILVA <linoxvictor@gmail.com>
2023-02-17 22:35:42 -0300

    Feito Conexão DAL

--- LendasClassic.DAL/Conexao.cs
+++ LendasClassic.DAL/Conexao.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace LendasClassic.DAL
 {
-    internal class Conexao
+    public class Conexao
     {
+        //variaveis
+        protected MySqlConnection conn;
+        protected MySqlCommand cmd;
+        protected MySqlDataReader dr;
+
+        //metodos
+        protected void Conectar()
+        {
+            try
+            {
+                conn = new MySqlConnection("Data Source=localhost; Initial Catalog= lendasclassic; Uid=root; Pwd= ;");
+                conn.Open();    
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        protected void Desconectar()
+        {
+            try
+            {
+            
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+
+
     }
 }
